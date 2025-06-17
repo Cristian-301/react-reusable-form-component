fb1571eb2264770753c4f8989c9bb9d44d9051e9
Cristian <72266894+Cristian-301@users.noreply.github.com>
2025-06-17 15:39:33 +0200

    update

--- components/formComponent/ReusableForm.jsx
+++ components/formComponent/ReusableForm.jsx
@@ -87,118 +87,126 @@ export default function ReusableForm({ fields, onSubmit, schema }) {
       className="w-full p-5 md:p-10 text-start"
     >
       {fields.map((field) => {
-        if (!shouldRender(field)) return null;
+  if (!shouldRender(field)) return null;
 
-        return (
-          <div key={field.name} className="mb-4 text-start">
-             {["text", "email", "password", "date"].includes(field.type) && (
+  return (
+    <div key={field.name} className="mb-4 text-start">
+      {field.type !== "checkbox" && field.type !== "rating" && (
+        <label
+          htmlFor={field.name}
+          className="mb-1 block text-sm font-medium text-gray-900"
+        >
+          {field.label}
+        </label>
+      )}
+
+      {["text", "email", "password", "date"].includes(field.type) && (
+        <input
+          id={field.name}
+          type={field.type}
+          {...register(field.name)}
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          placeholder={field.placeholder}
+        />
+      )}
+
+      {field.type === "textarea" && (
+        <textarea
+          id={field.name}
+          {...register(field.name)}
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          placeholder={field.placeholder}
+        />
+      )}
+
+      {field.type === "select" && (
+        <select
+          id={field.name}
+          {...register(field.name)}
+          className="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        >
+          {field.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      )}
+
+      {field.type === "radio" && (
+        <div className="flex flex-col gap-1 mt-1">
+          {field.options.map((option) => (
+            <label
+              key={option.value}
+              className="block text-sm font-medium text-gray-900"
+            >
               <input
-                type={field.type}
+                type="radio"
+                value={option.value}
                 {...register(field.name)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                placeholder={field.placeholder}
+                className="mr-2"
               />
-            )}
-             {field.type === "textarea" && (
-              <textarea
-                {...register(field.name)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                placeholder={field.placeholder}
-              />
-            )}
-            {field.type === "rating" && (
-              <div className="flex flex-col gap-1">
-                <span>{field.label}</span>
-                <div className="flex gap-1">
-                  {[...Array(field.max || 5)].map((_, i) => {
-                    const value = i + 1;
-                    return (
-                      <label key={value} className="cursor-pointer">
-                        <input
-                          type="radio"
-                          value={value}
-                          {...register(field.name)}
-                          className="hidden"
-                        />
-                        <span className="text-2xl text-yellow-400">
-                          {value <= (watch(field.name) || 0) ? "★" : "☆"}
-                        </span>
-                      </label>
-                    );
-                  })}
-                </div>
-                {errors[field.name] && (
-                  <div className="text-sm text-red-500">
-                    {errors[field.name].message}*
-                  </div>
-                )}
-              </div>
-            )}
-            {field.type === "checkbox" ? (
-              <label className="flex items-center gap-2 text-sm/6 font-medium text-gray-900">
-                <input
-                  type="checkbox"
-                  {...register(field.name)}
-                  className="mt-1 my-auto rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                />
-                {field.label}
-              </label>
-            ) : (
-              <label className="flex items-center gap-2 text-sm/6 font-medium text-gray-900">{field.label}</label>
-            )}
+              {option.label}
+            </label>
+          ))}
+        </div>
+      )}
 
-            {field.type === "select" && (
-              <select
-                {...register(field.name)}
-                className=" w-full  rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              >
-                {field.options.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            )}
+      {field.type === "checkbox" && (
+        <label className="flex items-center gap-2 text-sm font-medium text-gray-900">
+          <input
+            id={field.name}
+            type="checkbox"
+            {...register(field.name)}
+            className="rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          />
+          {field.label}
+        </label>
+      )}
 
-            {field.type === "radio" && (
-              <div className="flex gap-4 mt-1">
-                {field.options.map((option) => (
-                  <label
-                    key={option.value}
-                    className="block font-medium text-gray-900"
-                  >
-                    <input
-                      type="radio"
-                      value={option.value}
-                      {...register(field.name)}
-                      className="mr-2"
-                    />
-                    {option.label}
-                  </label>
-                ))}
-              </div>
-            )}
+      {field.type === "file" && (
+        <input
+          id={field.name}
+          type="file"
+          {...register(field.name)}
+          className="mt-1 w-full text-sm text-gray-700"
+        />
+      )}
 
-           
-
-            {field.type === "file" && (
-              <input
-                type="file"
-                {...register(field.name)}
-                className="mt-1 text-slate-50 w-full"
-              />
-            )}
-
-           
-
-            {errors[field.name] && (
-              <div className="text-sm text-red-500">
-                {errors[field.name].message}
-              </div>
-            )}
+      {field.type === "rating" && (
+        <div className="flex flex-col gap-1">
+          <label className="mb-1 block text-sm font-medium text-gray-900">
+            {field.label}
+          </label>
+          <div className="flex gap-1">
+            {[...Array(field.max || 5)].map((_, i) => {
+              const value = i + 1;
+              return (
+                <label key={value} className="cursor-pointer">
+                  <input
+                    type="radio"
+                    value={value}
+                    {...register(field.name)}
+                    className="hidden"
+                  />
+                  <span className="text-2xl text-yellow-400">
+                    {value <= (watch(field.name) || 0) ? "★" : "☆"}
+                  </span>
+                </label>
+              );
+            })}
           </div>
-        );
-      })}
+        </div>
+      )}
+
+      {errors[field.name] && (
+        <div className="mt-1 text-sm text-red-500">
+          {errors[field.name].message}
+        </div>
+      )}
+    </div>
+  );
+})}
 
       <button
         type="submit"
